35179bf525e68e2256e67810b75d7c7fdd02632f
Kappy.IO <kappy@kaplowitz.net>
2018-01-01 17:03:46 +0000

    removing ubuntu-specific items

--- dot_boot.sh
+++ dot_boot.sh
@@ -3,19 +3,6 @@
 # 2: clone personal dotfiles repo
 # 3: symlink dotfiles
 
-declare -a apps_arr=( "ack-grep"
-                      "git" 
-		      "zsh" 
-                    )
-
-for i in "${apps_arr[@]}"
-do
-	if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok $i installed") -eq 0 ]; then
-            echo "installing $i"
-	    sudo apt-get install $i
-	fi
-done
-
 if [ -f ~/.dotfiles ]; then
     echo "dotfiles dir found"
 else
