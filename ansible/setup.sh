158f98dc4edbc7edb648d65a757ab9e35ee9da3a
David Kaplowitz <david.kaplowitz@villanova.edu>
2018-06-30 23:21:49 -0400

    corrections

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# wget -O- https://raw.githubusercontent.com/vudkapz/dotfiles/master/ansible/setup.sh | sh
+# wget -O- https://raw.githubusercontent.com/kappyio/dotfiles/master/ansible/setup.sh | sh
 sudo apt-get install -y git python-pip python-dev
 sudo pip install ansible
-git clone https://github.com/vudkapz/dotfiles.git
+git clone https://github.com/kappyio/dotfiles.git
 cd dotfiles
 ansible-playbook installation.yml --sudo -K -c local -i "localhost,"
