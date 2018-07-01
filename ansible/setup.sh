a4c554022fb7df1a052f331b1b0e24395756b90b
David Kaplowitz <david.kaplowitz@villanova.edu>
2018-07-01 00:02:42 -0400

    remove sudo entirely

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # wget -O- https://raw.githubusercontent.com/kappyio/dotfiles/master/ansible/setup.sh | sh
-mkdir -p ~/.cache/pip/http
 sudo apt-get install -y git python-pip python-dev
-sudo pip install ansible
+pip install ansible
 git clone https://github.com/kappyio/dotfiles.git
 cd dotfiles/ansible
 ansible-playbook install.yml --become -K -c local -i "localhost,"
