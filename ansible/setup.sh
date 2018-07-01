aa471ff84b3154b2b3660308284aa159ae314226
David Kaplowitz <david.kaplowitz@villanova.edu>
2018-06-30 23:49:06 -0400

    creating cache dir that pip keeps complaining about

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # wget -O- https://raw.githubusercontent.com/kappyio/dotfiles/master/ansible/setup.sh | sh
-mkdir -p ~/.cache/pip
+mkdir -p ~/.cache/pip/http
 sudo apt-get install -y git python-pip python-dev
 sudo pip install ansible
 git clone https://github.com/kappyio/dotfiles.git
